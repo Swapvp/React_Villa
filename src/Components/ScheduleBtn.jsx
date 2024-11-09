3c9a7ceb0e7ccd0a5d6760ee34c002ae407934c1
Swapvp <swapneshpawar1994@gmail.com>
2024-11-09 16:32:21 +0530

    schedule btn css updated

--- src/Components/ScheduleBtn.jsx
+++ src/Components/ScheduleBtn.jsx
@@ -3,8 +3,8 @@ import { SlCalender } from "react-icons/sl";
 
 function ScheduleBtn() {
   return (
-    <button className="bg-black relative rounded-3xl text-white pl-10 py-3 w-52">
-      <div className="absolute bg-red-400 w-[25%] h-full text-center rounded-full top-0 left-0">
+    <button className="bg-black relative rounded-3xl text-white pl-3 py-2 w-48">
+      <div className="absolute bg-red-400 w-1/5 h-full text-center rounded-full top-0 left-0">
         <SlCalender className=" text-xl font-bold" />
       </div>
       Schedule a visit
